03d9c31cabaa42113cfcb7fcebcc43b06ca3432a
RumiYo <yokoyamarumi@gmail.com>
2023-05-24 00:22:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);   
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const appendCats = [...cats, name];
+    return appendCats;
+}
+function prependCat(name){
+    const prependCats = [name, ...cats];
+    return prependCats;
+}
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
